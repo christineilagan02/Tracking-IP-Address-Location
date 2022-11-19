1d5650304fc2e14da92c24079187706affc4f85b
unknown <christineilagan02@gmail.com>
2022-11-19 17:14:04 +0800

    Last is adding a pin for location and a circle mark

--- IPMap.py
+++ IPMap.py
@@ -1,6 +1,15 @@
 import geocoder
+import folium
 
 g = geocoder.ip("me")
 Address = g.latlng
 
-print(Address)
+my_map1 = folium.Map(location=Address,
+                     zoom_start=12)
+
+folium.CircleMarker(location=Address, 
+                    radius=50, popup="location").add_to(my_map1)
+
+folium.Marker(Address, popup="location").add_to(my_map1)
+
+my_map1.save("my_map.html")
